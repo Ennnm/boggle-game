3e7e2338c2b838ab6c033e2a3e2e8288e10a20d7
ennnm <jiaen.1sc4@gmail.com>
2022-02-08 12:17:52 +0800

    test on posting json is ok

--- controllers/games.mjs
+++ controllers/games.mjs
@@ -1,9 +1,19 @@
-import * as board from '../lib/board.mjs';
+import * as boardFunc from '../lib/board.mjs';
 
 export default function initGamesController(db) {
   const create = async (req, res) => {
     const { duration, random, board } = req.body;
+    console.log('req.body :>> ', req.body);
 
+    const reqBody = {
+      duration: 1234,
+      random: false,
+      board: 'A, C, E, D, L, U, G, *, E, *, H, T, G, A, F, K',
+    };
+    const randomBoard = boardFunc.genRandomBoard();
+    const boardArr2 = boardFunc.convertArr1ToSquareArr(randomBoard);
+    console.log('randomBoard :>> ', randomBoard);
+    res.status(201).send(boardArr2);
     // return response status 201
     //     {
     //   "id": 1,
--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-// import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import methodOverride from 'method-override';
 
 import bindRoutes from './routes.mjs';
@@ -7,12 +7,13 @@ import bindRoutes from './routes.mjs';
 // Initialise Express instance
 const app = express();
 // Set the Express view engine to expect EJS templates
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 // Bind cookie parser middleware to parse cookies in requests
-// app.use(cookieParser());
+app.use(cookieParser());
 // Bind Express middleware to parse request bodies for POST requests
 app.use(express.urlencoded({ extended: false }));
 // Bind method override middleware to parse PUT and DELETE requests sent as POST requests
+app.use(express.json());
 app.use(methodOverride('_method'));
 // Expose the files stored in the public folder
 app.use(express.static('public'));
