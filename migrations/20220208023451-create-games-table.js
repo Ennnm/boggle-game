e221ecedaeeb3ddcd2719fa48fb3cd245457b8b0
ennnm <jiaen.1sc4@gmail.com>
2022-02-08 10:43:27 +0800

    init game migration

---
+++ migrations/20220208023451-create-games-table.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      token: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      duration: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      board: {
+        allowNull: false,
+        type: Sequelize.STRING(64),
+      },
+      points: {
+        type: Sequelize.INTEGER,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('games');
+  },
+};
