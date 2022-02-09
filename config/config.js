ed81d2550e25315c73466e3195f135369a52bf6c
ennnm <jiaen.1sc4@gmail.com>
2022-02-09 21:16:01 +0800

    added production config

--- config/config.js
+++ config/config.js
@@ -6,4 +6,11 @@ module.exports = {
     host: '127.0.0.1',
     dialect: 'postgres',
   },
+  production: {
+    username: 'postgres',
+    password: process.env.DB_PASSWORD,
+    database: 'boggle_game',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
 };
